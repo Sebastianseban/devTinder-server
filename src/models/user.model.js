aec871d546368b46b5b7798a7a1a1af4842680fc
sebastian <sebastianseban0105@gmail.com>
2025-08-25 17:58:57 +0530

    feat: complete user authentication with password hashing and JWT tokens
    - Fix userSchema.methods typo (was userSchema.method)
    - Add password hashing with bcrypt pre-save middleware
    - Add isPasswordCorrect method for password verification
    - Add generateAccessToken and generateRefreshToken methods
    - Include proper validation for email, password, and skills

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -52,5 +53,43 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+        _id: this._id,
+      emailId: this.emailId,
+      firstName: this.firstName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY || "1h",
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+       _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY || "7d",
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
