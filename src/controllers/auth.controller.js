fd3db395823e6e041b6abf8245fd9ae52c1551e3
sebastian <sebastianseban0105@gmail.com>
2025-08-27 16:28:50 +0530

    feat(auth): add login API with rate limiting to prevent brute-force attacks

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -86,7 +86,47 @@ export const registerUser = asyncHandler(async (req, res) => {
           user: createdUser,
           accessToken,
         },
-        "User registered and logged in successfully" 
+        "User registered and logged in successfully"
       )
     );
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { emailId, password } = req.body;
+
+  if (!emailId || !password) {
+    throw new ApiError(400, "Required fields are missing", [
+      { field: "emailId", message: "Email is required" },
+      { field: "password", message: "Password is required" },
+    ]);
+  }
+
+  const user = await User.findOne({ emailId });
+
+  if (!user) {
+    throw new ApiError(404, "user does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  };
+
+  return res.status(200).cookie("refreshToken", refreshToken, options).json(new ApiResponse(200,{user:loggedInUser,accessToken,},"User logged in Successfully"));
 });
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { loginUser, registerUser } from "../controllers/auth.controller.js";
+import { loginRateLimiter } from "../middleware/rateLimiter.middleware.js";
 
 const router = Router();
 
 router.post("/signup", registerUser);
+router.post("/login",loginRateLimiter, loginUser);
 
 export default router;
