0274e3b5bd59b45b79bf1d85774699133dfbe76a
sebastian <sebastianseban0105@gmail.com>
2025-08-29 15:09:15 +0530

    feat/created refreshtoken api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -161,3 +162,61 @@ export const logoutUser = asyncHandler(async (req, res) => {
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User Logged Out"));
 });
+
+export const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "refresh token is expired or used");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, NewRefreshToken } =
+      await generateAccessAndRefreshToken(user._id);
+
+    return res
+      .status(200)
+      .cookie("refreshToken", NewRefreshToken, options)
+      .json(new ApiResponse(200, { accessToken }, "Access token refreshed"));
+  } catch (error) {
+    throw new ApiError(401, error?.message || "invalid refresh token");
+  }
+});
+
+export const getCurrentUser = asyncHandler(async (req,res) => {
+  const userId = req.user?.id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized access");
+  }
+
+  const user = await User.findById(userId).select("-password -refreshToken" )
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, "User fetched successfully"));
+})
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,20 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js";
+import {
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/auth.controller.js";
 import { loginRateLimiter } from "../middleware/rateLimiter.middleware.js";
-import {verifyJWT} from "../middleware/auth.Middleware.js"
+import { verifyJWT } from "../middleware/auth.Middleware.js";
 
 const router = Router();
 
 router.post("/signup", registerUser);
-router.post("/login",loginRateLimiter, loginUser);
-router.post("/logout",verifyJWT,logoutUser)
+router.post("/login", loginRateLimiter, loginUser);
+router.post("/logout", verifyJWT, logoutUser);
+router.route("/refresh-token").get(refreshAccessToken);
+router.route("/me").get(verifyJWT, getCurrentUser);
 
 export default router;
